68e60be8967d033b31566497bd832f2bea4d3779
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-06-30 16:26:22 -0300

    Calculadora simples em Dart

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,5 +1,36 @@
-import 'package:meu_app/meu_app.dart' as meu_app;
+import 'dart:io';
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  print("Bem vindo a nossa calculadora!");
+  print("Informe o primeiro numero:");
+  var line = stdin.readLineSync(encoding: utf8);
+  var numero1 = double.parse(line ?? "0");
+
+  print("Informe o segundo numero:");
+  line = stdin.readLineSync(encoding: utf8);
+  var numero2 = double.parse(line ?? "0");
+
+  print("Informe a operação matemática (+, -, *, /)");
+  line = stdin.readLineSync(encoding: utf8);
+  var operacao = line ?? "0";
+
+  switch (operacao) {
+    case "+": 
+      print(numero1 + numero2);
+      break;
+    case "-":
+      print(numero1 - numero2);
+      break;
+    case "*":
+      print(numero1*numero1);
+      break;
+    case "/":
+      print(numero1 / numero2);
+      break;
+    default:
+      print("A operação selecionada não existe");
+      break;
+  }
+  
 }
